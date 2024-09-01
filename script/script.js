b6b16e8dcc98cad2e24f0bf36f3091ae029f1798
Mohit Vaishnav <mr.mohitvaishnav56@gmail.com>
2024-09-01 16:33:39 +0530

    add Rating and Review section

--- script/script.js
+++ script/script.js
@@ -101,3 +101,17 @@ scrollTexts.forEach(function(slide, index) {
         opacity: 1,
     }); // "<" syncs this animation with the previous one
 });
+
+var swiper = new Swiper(".mySwiper", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2000,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+
+  });
