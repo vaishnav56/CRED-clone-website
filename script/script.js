193325793486ad5263c530667429fe66aca51a5d
Mohit Vaishnav <mr.mohitvaishnav56@gmail.com>
2024-09-01 08:02:44 +0530

    animation added

--- script/script.js
+++ script/script.js
@@ -59,4 +59,45 @@ tl.from("#img5",{
 })
 
 
+const tl2 = gsap.timeline({
+    scrollTrigger: {
+        trigger: ".part5",
+        scroller: "body",
+        start: "top 0%",
+        end: "bottom -100%",
+        scrub: 2,
+        pin: true,
+    }
+});
 
+var images = document.querySelectorAll(".phone img");
+var scrollTexts = document.querySelectorAll(".scroll-text");
+
+scrollTexts.forEach(function(slide, index) {
+    // Animate the scroll texts
+    
+
+    // Animate the corresponding image; index+1 checks for out-of-bound error
+    if (index + 1 < scrollTexts.length) {
+        tl2.to(scrollTexts[index], {
+            marginTop: `-100%`,
+            opacity: 1,
+        });
+        };
+    
+    if (index + 1 < images.length) {
+        tl2.to(images[index + 1], {
+            marginLeft: "-100%",
+            opacity: 1,
+        });
+    }
+
+    // Fade out the current image
+    tl2.to(images[index], {
+        opacity: 0,
+    }, "<");
+
+    tl2.to(images[images.length - 1], {
+        opacity: 1,
+    }); // "<" syncs this animation with the previous one
+});
